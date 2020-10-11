7c44a63d8f716ef31763b91c0c0956b2daec84b0
Mai Pham <maitphm@gmail.com>
2020-10-11 03:29:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["wow", "i", "am", "really", "learning"] << element
+  array = ["wow", "i", "am", "really", "learning"].tolast(element)
   element = "arrays!"
 end
 
