b93575b38b4ef16b9cac97d65b53db2f42195a86
Mai Pham <maitphm@gmail.com>
2020-10-11 03:14:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  [] 
 end
 
 def create_an_array
-  
+  arr = ["dogs", "cats", "birds"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  arr = ["dogs", "cats", "birds"]
+  arr << "fish"
 end
 
 def add_element_to_start_of_array(array, element)
