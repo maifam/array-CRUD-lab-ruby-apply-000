8573de7d9b94253f4acc32cac7b72acb677ef11c
Mai Pham <maitphm@gmail.com>
2020-10-11 03:26:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element
+  array = ["dogs", "cats", "birds", "fish"] << element
+  
 end
 puts array(["dogs", "cats", "birds", "fish"], "snake")
 
